ebdfa1246de278a3341a1f8975884b6f9bd3a45a
felipeazs <felipeazs@gmail.com>
2022-08-30 07:54:33 -0400

    fetch de datos desde https://mindicador.cl/api con useEffect. Definición inicial del estado del indicador a 'bitcoin' y obtención de datos de los últimos 30 puntos del año 2022. Creación de los estados 'valores' y 'loading'. Despliegue de valores.

--- src/components/MyApi.jsx
+++ src/components/MyApi.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Chart from './Chart';
 
+const baseUrl = 'https://mindicador.cl/api';
+
 const MyApi = () => {
+	const [indicador, setIndicador] = useState('bitcoin');
+	const [valores, setValores] = useState([]);
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		const fetchDatosIndicador = async () => {
+			const response = await fetch(`${baseUrl}/${indicador}/2022`);
+			const data = await response.json();
+
+			const arr = data.serie.reverse().slice(-30);
+
+			setLoading(false);
+			setValores(arr);
+
+			console.log(arr);
+		};
+
+		fetchDatosIndicador();
+	});
+
+	const listaValores = valores.map((valor) => <li>{valor.valor}</li>);
 	return (
 		<div>
+			<ul>{listaValores}</ul>
 			<Chart />
 		</div>
 	);
