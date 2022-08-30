0b846e61545ae93e59b96fd8fba7639a5c2c3a79
felipeazs <felipeazs@gmail.com>
2022-08-30 09:14:00 -0400

    Incorporación del select para cambiar el indicador a mostrar. Manejo de seleccionadores con useState y un nuevo useEffect

--- src/components/MyApi.jsx
+++ src/components/MyApi.jsx
@@ -6,25 +6,75 @@ const baseUrl = 'https://mindicador.cl/api';
 
 const MyApi = () => {
 	const [indicador, setIndicador] = useState('bitcoin');
-	const [valores, setValores] = useState([]);
+	const [valoresIndicador, setValoresIndicador] = useState([]);
+	const [seleccionadores, setSeleccionadores] = useState([]);
 	const [loading, setLoading] = useState(true);
+
+	//Obtener todos los datos de la api para generar la selección dinámica de indicadores
 	useEffect(() => {
-		const fetchDatosIndicador = async () => {
-			const response = await fetch(`${baseUrl}/${indicador}/2022`);
+		const fetchTodosLosDatos = async () => {
+			const response = await fetch(baseUrl);
 			const data = await response.json();
 
-			const arr = data.serie.reverse().slice(-30);
+			if (data) {
+				const sel = [];
+				for (const d in data) {
+					if (data[d]['codigo']) {
+						sel.push(data[d]['codigo']);
+					}
+				}
+				setSeleccionadores(sel);
+			}
+		};
+
+		fetchTodosLosDatos();
+	}, []);
+
+	//Obtener valores según el indicador del presente año
+	useEffect(() => {
+		const fetchDatosIndicador = async () => {
+			setLoading(true);
+			const anio = new Date().getFullYear();
+			const response = await fetch(`${baseUrl}/${indicador}/${anio}`);
+			const data = await response.json();
+
+			const ultimosDatos = data.serie.reverse().slice(-30);
 
 			setLoading(false);
-			setValores(arr);
+			setValoresIndicador(ultimosDatos);
 		};
 
 		fetchDatosIndicador();
-	}, []);
+	}, [indicador]);
+
+	const indicadorHandler = (event) => {
+		setIndicador(event.target.value);
+	};
+
+	const listaSeleccionadores = seleccionadores.map((seleccionador, i) => (
+		<option
+			value={seleccionador}
+			key={i}
+		>
+			{seleccionador}
+		</option>
+	));
 
 	return (
 		<div>
-			<Chart indicadores={valores} />
+			<div>
+				<label htmlFor='indicador'>Ingresa el indicador</label>
+				<select
+					name='indicador'
+					id='indicador'
+					value={indicador}
+					onChange={indicadorHandler}
+				>
+					{listaSeleccionadores}
+				</select>
+			</div>
+			{loading && <h2>Cargando datos del indicador...</h2>}
+			<Chart indicadores={valoresIndicador} />
 		</div>
 	);
 };
